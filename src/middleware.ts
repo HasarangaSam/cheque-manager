9a824be1830f22a864058fb2691f10f38075839f
Hasaranga <hasarangasamarakoon@gmail.com>
2026-08-25 19:25:00 +0530

    Add clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
+  "/__clerk(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
@@ -14,7 +15,8 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    // Always run for API routes and Clerk proxy routes
+    "/(api|trpc|__clerk)(.*)",
   ],
 };
+
